1472ee4c759f43aea299a17667db7b314ce32917
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-11-26 15:19:16 -0500

    LaTeX template 2

---
+++ writing/20xx-confName-shortDescription/biblio/README.md
@@ -0,0 +1,7 @@
+# Description
+Collection of all biblio files for citations loosely organized by topics.
+
+# Content
+ *.bib		BibTeX files
+ ieee.bst	Standard BibTeX style for IEEE
+
---
+++ writing/20xx-confName-shortDescription/biblio/auto/websites.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook
+ "websites"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "ArrayOfThings"
+    "SONYC"
+    "parallemic"
+    "Sookraj"
+    "NetMatch"
+    "Duckietown"
+    "DJITelloEdu"
+    "BUTodayIntroductionToRobotics"
+    "WeaselWords"
+    "WidowOrphanLines"))
+ :bibtex)
+
