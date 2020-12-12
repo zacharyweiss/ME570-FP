b125c04256e4e6f9b267cefd1b19873f84583439
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-12-12 14:13:08 -0500

    Nearest neighbor via min vecnorm

--- generate_rrt.m
+++ generate_rrt.m
@@ -2,10 +2,10 @@ function tree = generate_rrt(world,xStart,k,deltaT)
     tree = tree_initialize(xStart,k);
     for vertex = 1:k
         xRand = random_state(world);
-        xNear = nearest_neighbor(tree,xRand);
-        u = select_input(world,xRand,xNear);
-        xNew = new_state(xNear,u,deltaT);
+        idxNear = nearest_neighbor(tree,xRand);
+        u = select_input(world,xRand,idxNear);
+        xNew = new_state(idxNear,u,deltaT);
         tree = tree_addVertex(tree,xNew);
-        tree = tree_addEdge(tree,xNear,xNew,u);
+        tree = tree_addEdge(tree,idxNear,xNew,u);
     end
 end
--- nearest_neighbor.m
+++ nearest_neighbor.m
@@ -1,3 +1,4 @@
-function xNear = nearest_neighbor(xRand,tree)
-    
+function idxNear = nearest_neighbor(tree,xRand)
+% using vector norm, find nearest node on tree to sampled point
+    [~,idxNear] = min(vecnorm([tree.x]-xRand));
 end
