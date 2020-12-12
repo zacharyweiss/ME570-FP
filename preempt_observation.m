a4b5447d1f3ae7d200e40a97f6a87ddbdb9ff195
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-12-12 17:35:44 -0500

    Preempt function structure

---
+++ preempt_observation.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = preempt_observation(inputArg1,inputArg2)
+%PREEMPT_OBSERVATION Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
--- preempt_path.m
+++ preempt_path.m
@@ -1,11 +1,11 @@
-function rrt_preemptive()
+function preempt_path()
     nVerts = 250;
     deltaT = 0.1;
     
     load polygonWorld.mat
+    tree = rrt_generate(world,xStart(:,1),nVerts,deltaT);
     
     clf
-    tree = rrt_generate(world,xStart(:,1),nVerts,deltaT)
     axis equal
     rrt_plot(world,tree)
     
