ba8fc7a27ad914001df42a037597b142b02518b2
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-12-13 15:45:15 -0500

    Observations structure + calculation at node level

--- rrt_generate.m
+++ rrt_generate.m
@@ -8,6 +8,16 @@ function tree = rrt_generate(world,xStart,k,deltaT)
             u = select_input(xRand,tree(idxNear).x,deltaT);
             xNew = state_new(tree(idxNear).x,u,deltaT);
             isValidConfig = state_new_isValid(world,xNew,tree(idxNear).x);
+            % collision observations, increment for idxNear and children
+            if ~isValidConfig
+                idxChildren = idxNear;
+                while ~isempty(idxChildren)
+                    tree(idxChildren(1)).obs.col = tree(idxChildren(1)).obs.col + 1;
+                    idxSubChild = tree(idxChildren(1)).children;
+                    idxChildren = [idxChildren idxSubChild];
+                    idxChildren(1) = [];
+                end
+            end
         end
         tree = tree_makeChild(tree,iVertex,xNew,idxNear);
     end
--- tree_initialize.m
+++ tree_initialize.m
@@ -1,5 +1,8 @@
 function tree = tree_initialize(xStart,k)
-    tree = repmat(struct('parent',[],'children',[],'edge',[],'x',[]),k,1);
+    tree = repmat(struct('parent',[],'children',[],'edge',[],'x',[],'obs',struct('nodes',-1,'dist',-1,'col',-1)),k,1);
     tree(1).parent = 0;
     tree(1).x = xStart;
+    
+    % observations
+    tree(1).obs.nodes = 1;
 end
