2408041318a53271086796d4fe38667bc9e4397e
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-12-12 19:26:13 -0500

    Preemption function for hypothesis trimming

--- preempt_score.m
+++ preempt_score.m
@@ -1,7 +1,4 @@
-function [outputArg1,outputArg2] = preemption_scoring(inputArg1,inputArg2)
-%PREEMPTION_SCORING Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function hypothesisLogLikelyhood = preempt_score(hypothesis,observations)
+    
 end
 
