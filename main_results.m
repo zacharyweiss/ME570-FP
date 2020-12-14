88b871fc4e6c173a3f46cbeedcad259ecf7f2a53
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-12-14 15:07:34 -0500

    Example plots, thin nonconvex world

--- main_results.m
+++ main_results.m
@@ -2,9 +2,9 @@
 load('polygonWorld.mat','world','xGoal','xStart')
 
 % settings
-nVerts = 200;   % number of vertices to build tree to
-deltaT = 0.05;   % stepsize of tree
-iStart = 1;     % start position, anywhere from 1-5
+nVerts = 200;   % number of vertices to build tree to before preempting
+deltaT = 0.05;   % stepsize of tree, best on order of 0.01-0.1
+iStart = 1;      % start position, anywhere from 1-5
 
 % generate RRT with observation metrics for preemption
 tree = rrt_generate(world,xStart(:,iStart),nVerts,deltaT);
@@ -13,7 +13,7 @@ rrt_plot(world,tree)
 
 % find preemptive best path at time of nVerts cutoff
 xPath = preempt_path(tree,xGoal);
-% overlay path and goal on plot
+% overlay path, goal, and shortest path w/o obstacles on plot
 hold on
 plot(xPath(1,:),xPath(2,:),'g-')
 plot([xPath(1,1) xGoal(1)],[xPath(2,1) xGoal(2)],'b-')
