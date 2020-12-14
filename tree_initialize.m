8f1cbf1d8b6448434dbf1b60c15bafae706c6b80
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-12-13 23:57:36 -0500

    Observations pt. 2

--- tree_initialize.m
+++ tree_initialize.m
@@ -1,9 +1,11 @@
 function tree = tree_initialize(xStart,k)
-    tree = repmat(struct('parent',[],'children',[],'edge',[],'x',[],'obs',struct('idx',-1,'nodes',-1,'dist',-1,'col',-1)),k,1);
+    tree = repmat(struct('parent',[],'children',[],'edge',[],'x',[],'obs',struct('idx',[],'nodes',[],'dist',[],'col',[])),k,1);
     tree(1).parent = 0;
     tree(1).x = xStart;
     
     % observations
     tree(1).obs.idx = 1;
     tree(1).obs.nodes = 1;
+    tree(1).obs.dist = 0;
+    tree(1).obs.col = 0;
 end
--- tree_makeChild.m
+++ tree_makeChild.m
@@ -1,6 +1,6 @@
 function tree = tree_makeChild(tree,iVertex,xNew,idxNear)
     tree(iVertex).x = xNew;
-    tree(iVertex).edge = xNew - tree(idxNear).x;
+    tree(iVertex).edge = xNew-tree(idxNear).x;
     tree(iVertex).parent = idxNear;
     tree(idxNear).children = [tree(idxNear).children iVertex];
     
